b33e31721254c13d8ae429dcc0e748d02b48c54f
Jhuliana <jholeanasantos@gmail.com>
2022-05-11 00:34:01 -0300

    Layout

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import StartScreen from './StartScreen';
+import DeckScreen from './DeckScreen';
+
+export default function App() {
+    return (
+        <>
+            <StartScreen />
+            {/* <DeckScreen /> */}
+        </>
+    )
+}
